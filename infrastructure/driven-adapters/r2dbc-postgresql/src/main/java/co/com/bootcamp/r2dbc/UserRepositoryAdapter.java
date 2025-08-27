0f9dcb9c503d59cca714b97448116b5f021d9b96
Arley Romero Murcia <arome18@bancodebogota.com.co>
2025-08-27 18:07:21 -0500

    Add transaction

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bootcamp/r2dbc/UserRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bootcamp/r2dbc/UserRepositoryAdapter.java
@@ -7,19 +7,23 @@ import co.com.bootcamp.r2dbc.helper.ReactiveAdapterOperations;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Repository
 public class UserRepositoryAdapter extends ReactiveAdapterOperations<User, UserEntity, String, UserReactiveRepository> implements UserRepository {
 
-    public UserRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper) {
+    private final TransactionalOperator transactionalOperator;
+
+    public UserRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper, TransactionalOperator transactionalOperator) {
         super(repository, mapper, d -> mapper.map(d, User.class));
+        this.transactionalOperator = transactionalOperator;
     }
 
     @Override
     public Mono<User> createUser(User user) {
-        return super.save(user);
+        return super.save(user).as(transactionalOperator::transactional);
     }
 
     @Override
