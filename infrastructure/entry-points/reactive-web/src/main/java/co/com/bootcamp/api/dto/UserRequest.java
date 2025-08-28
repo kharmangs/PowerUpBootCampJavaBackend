15c396b9eb30b7ffe1414f370f88101a62c9e4f2
Arley Romero Murcia <arome18@bancodebogota.com.co>
2025-08-27 19:09:17 -0500

    Generic errors

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bootcamp/api/dto/UserRequest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bootcamp/api/dto/UserRequest.java
@@ -4,9 +4,7 @@ import lombok.NonNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public record UserRequest(
         String id,
@@ -23,20 +21,9 @@ public record UserRequest(
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
 
     public UserRequest {
-        Map<String, String> requireFields = Map.of(
-                "names", names,
-                "surnames", surnames,
-                "identification", identification,
-                "email", email
-        );
 
-        String blankFields = requireFields.entrySet().stream()
-                .filter(e -> e.getValue().isBlank())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.joining(", "));
-
-        if (!blankFields.isEmpty()) {
-            throw new IllegalArgumentException("The following fields cannot be blank: " + blankFields);
+        if (names.isBlank() || surnames.isBlank() || email.isBlank()) {
+            throw new IllegalArgumentException("The fields cannot be blank");
         }
 
         if (salary.compareTo(BigDecimal.ZERO) <= 0 || salary.compareTo(SALARY_LIMIT) > 0) {
